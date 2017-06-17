e6821c2e1e36b0a03548efcb30da6dbb0967f425
Daniel Coulbourne <daniel@tighten.co>
2017-06-17 01:06:29 -0400

    Hitch your wagon to a NPM package.

---
+++ Resource.js
@@ -0,0 +1,58 @@
+/***
+*
+* EXAMPLE USAGE
+*
+* Resource.index('users')
+* Resource.store('users', { email: 'daniel@tighten.co' })
+* Resource.show('users', 1)
+* Resource.update('users', { id: 1, email: 'daniel.coulbourne@gmail.com' })
+* Resource.destroy('users', 1)
+*
+***/
+
+
+class Resource {
+    constructor() {
+        this.url = '/api';
+    }
+
+    index(resource) {
+        return this.http('GET', resource);
+    }
+
+    store(resource, payload) {
+        return this.http('POST', resource, payload);
+    }
+
+    show(resource, id) {
+        return this.http('GET', resource, null, id);
+    }
+
+    update(resource, payload) {
+        return this.http('PUT', resource, payload, payload.id);
+    }
+
+    destroy(resource, id) {
+        return this.http('DELETE', resource, null, payload.id);
+    }
+
+    http(method, resource, payload = null, path = '') {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: method,
+                url: `${ this.url }/${ resource }/${ path }`,
+                headers: {
+                    'Accept': 'application/json',               // I'm assuming your token is in App.apiToken
+                    'Authorization': 'Bearer ' + App.apiToken   // Do what you gotta do for your own Auth.
+                },
+                data: payload
+            }).then(response => {
+                return resolve(response.data);
+            }).catch(error => {
+                reject({ error });
+            });
+        });
+    }
+}
+
+export default new Resource();
